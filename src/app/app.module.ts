b189c8ef27428882dece9da1ea214b1f956d4285
gustavoafo <gustavoafonso.gu@gmail.com>
2018-04-19 14:11:38 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FirebaseConfig } from './../environments/firebase.config';
+import { AngularFireModule } from 'angularfire2/index';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(FirebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const FirebaseConfig = {
+    apiKey: 'AIzaSyAuUTUrNt3mB56Jl9YQhrLfiK33CoVj33Y',
+    authDomain: 'databasebureauuna.firebaseapp.com',
+    databaseURL: 'https://databasebureauuna.firebaseio.com',
+    projectId: 'databasebureauuna',
+    storageBucket: 'databasebureauuna.appspot.com',
+    messagingSenderId: '715398045992'
+  };
+
